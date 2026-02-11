7e29fb281656ef6689983f397d835dbea7c3421b
AdityaNayak12 <adinayak18@gmail.com>
2026-02-11 15:42:46 +0530

    feat: navbar

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">MyBrand</div>
+            <div className="navbar-links">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
+
+
+
